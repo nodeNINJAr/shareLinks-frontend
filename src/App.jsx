d1b9835444154c92045de7826653fb0c7f55c2e3
Mehedi hasan <nodeNINJAr@users.noreply.github.com>
2025-03-01 22:23:29 +0600

    project-setup-and-structure-all-file-and-route-make

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Outlet } from 'react-router'
+
+const App = () => {
+  return (
+       <div>
+          {/* main */}
+          <main>
+              <Outlet/>
+          </main>
+       </div>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,24 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router";
+import "./index.css";
+import App from "./App.jsx";
+import Home from "./pages/Home.jsx";
+import Login from "./pages/Login.jsx";
+import Register from "./pages/Register.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<Home />} />
+        </Route>
+        {/* auth */}
+        <Route path="auth/login" element={<Login/>} />
+        <Route path="auth/register" element={<Register />} />
+        {/*  */}
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
